9d11593be40085cefbee6fb4c6fc3b61014bdbea
Hiro5409 <delamtt28@gmail.com>
2021-04-10 15:36:18 +0900

    Create Fundraiser instance

--- contracts/FundraiserFactory.sol
+++ contracts/FundraiserFactory.sol
@@ -2,4 +2,31 @@
 
 pragma solidity 0.6.8;
 
-contract FundraiserFactory {}
+import "./Fundraiser.sol";
+
+contract FundraiserFactory {
+    Fundraiser[] private _fundraisers;
+
+    function createFundraiser(
+        string memory name,
+        string memory url,
+        string memory imageURL,
+        string memory description,
+        address payable beneficiary
+    ) public {
+        Fundraiser fundraiser =
+            new Fundraiser(
+                name,
+                url,
+                imageURL,
+                description,
+                beneficiary,
+                msg.sender
+            );
+        _fundraisers.push(fundraiser);
+    }
+
+    function fundraisersCount() public view returns (uint256) {
+        return _fundraisers.length;
+    }
+}
--- test/fundraiser_factory_test.js
+++ test/fundraiser_factory_test.js
@@ -1,8 +1,31 @@
 const FundraiserFactoryContract = artifacts.require("FundraiserFactory");
 
-contract("FundraiserFactory: development", accounts => {
+contract("FundraiserFactory: development", () => {
   it("it has been deployed", async () => {
     const fundraiserFactory = FundraiserFactoryContract.deployed();
     assert(fundraiserFactory, "fundraiser factory was not deployed");;
+  });
+});
+
+contract("FundraiserFactory: createFundraiser", accounts => {
+  const name = "Hiro Miya";
+  const url = "google.com";
+  const imageURL = "https://placekitten.com/600/350";
+  const description = "fuck";
+  const beneficiary = accounts[1];
+
+  it("increases the fundraisersCount", async () => {
+    const fundraiserFactory = await FundraiserFactoryContract.deployed();
+    const currentCount = await fundraiserFactory.fundraisersCount();
+    await fundraiserFactory.createFundraiser(
+      name,
+      url,
+      imageURL,
+      description,
+      beneficiary,
+    );
+    const newCount = await fundraiserFactory.fundraisersCount();
+    const diff = newCount - currentCount;
+    assert.equal(diff, 1, "should increment by 1");
   });
 });
