2be3a964322aae34f3c4d1c053c81ce30160dfe0
Hiro5409 <delamtt28@gmail.com>
2021-04-10 14:11:09 +0900

    Use solidity 0.6.8

--- contracts/Fundraiser.sol
+++ contracts/Fundraiser.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
-pragma solidity >0.4.23 <0.7.0;
+pragma solidity 0.6.8;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
